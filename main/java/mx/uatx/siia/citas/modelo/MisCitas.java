8fdaa9e309ccdfb892bf3c57519b041e12563bc4
Alberto N.R <nochebeto12@gmail.com>
2022-05-23 00:41:02 -0500

    I semi finish section mis citas

--- main/java/mx/uatx/siia/citas/modelo/MisCitas.java
+++ main/java/mx/uatx/siia/citas/modelo/MisCitas.java
@@ -9,17 +9,43 @@ public class MisCitas {
     private String strEstatus;
     private String strUser;
 
-    public MisCitas(String strIdCita, String strDescripcionCita, String strRepuesta, String strFechaHoraReservada, String strEstatus, String strUser) {
-        this.strIdCita = strIdCita;
-        this.strDescripcionCita = strDescripcionCita;
+    private String area;
+    private String tramite;
+
+
+    public MisCitas(String strIdCita, String strUser, String strDescripcionCita, String strRepuesta, String strFechaHoraReservada, String strEstatus) {
+
+        String[] datosCita = strDescripcionCita.split(",");
+        String[] fechaHora = strFechaHoraReservada.split(",");
+
+        System.out.println("Split =>"+datosCita);
+
+        this.strIdCita = datosCita[3]+strIdCita+datosCita[4];
+        this.strDescripcionCita = "<b>Descripción: </b>"+datosCita[0]+"<br>"+"<b>Area: </b>"+datosCita[1]+"<br>"+"<b>Tramite: </b>"+datosCita[2];
         this.strRepuesta = strRepuesta;
-        this.strFechaHoraReservada = strFechaHoraReservada;
+        this.strFechaHoraReservada = fechaHora[0]+"<br>"+fechaHora[1];
         this.strEstatus = strEstatus;
-        this.strUser = strUser;
+        this.strUser =  strUser;
     }
 
     public String getStrIdCita() {
-        return strIdCita;
+        return "CI"+strIdCita;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getTramite() {
+        return tramite;
+    }
+
+    public void setTramite(String tramite) {
+        this.tramite = tramite;
     }
 
     public void setStrIdCita(String strIdCita) {
@@ -35,7 +61,7 @@ public class MisCitas {
     }
 
     public String getStrRepuesta() {
-        return strRepuesta;
+        return strRepuesta != null ? strRepuesta : " ";
     }
 
     public void setStrRepuesta(String strRepuesta) {
@@ -43,7 +69,8 @@ public class MisCitas {
     }
 
     public String getStrFechaHoraReservada() {
-        return strFechaHoraReservada;
+        String[] fechaHora = strFechaHoraReservada.split(",");
+        return fechaHora[0]+"\n"+fechaHora[1];
     }
 
     public void setStrFechaHoraReservada(String strFechaHoraReservada) {
@@ -56,5 +83,13 @@ public class MisCitas {
 
     public void setStrEstatus(String strEstatus) {
         this.strEstatus = strEstatus;
+    }
+
+    public String getStrUser() {
+        return strUser;
+    }
+
+    public void setStrUser(String strUser) {
+        this.strUser = strUser;
     }
 }
